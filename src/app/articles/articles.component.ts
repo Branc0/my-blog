d5dc74e0d981b4a930e16e5d8f32bd5eb645f550
Rafael Rocha de azevedo <rafaelrochadeazevedo@Mac-mini-de-Rafael.local>
2022-01-16 21:22:20 -0300

    feat: scully setup and creates first post

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.scss']
 })
-export class ArticlesComponent implements OnInit {
+export class ArticlesComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  posts: ScullyRoute[] = [];
+  private routeSub: Subscription | null = null;
+
+  constructor(private scullyServices: ScullyRoutesService) {}
 
   ngOnInit(): void {
+    this.routeSub = this.scullyServices.available$.subscribe(posts => {
+      this.posts = posts.filter(post => post.title)
+    })
   }
 
+  ngOnDestroy(): void {
+    this.routeSub!.unsubscribe();
+  }
 }
